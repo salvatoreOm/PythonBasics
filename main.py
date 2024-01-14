0f8ad4f0f1c368dc5b778a361705039fbbcc06c2
Om Parihar <141987998+salvatoreOm@users.noreply.github.com>
2024-01-14 14:14:16 +0530

    made some changes

---
+++ main.py
@@ -0,0 +1,24 @@
+from math import *
+print('Hey Wassup?')
+# No use of initializing the data types , it utilizes itself
+is_male = False
+char = 'c'
+age = "36"
+money = "3.2355"
+print("John is " + age + " years old and he have "+money+ " coins and he is a " + char )
+
+# we can use .lower() and .upper() and islower() and isupper() for cases in string
+# for length we use len(string name) and for grabbing characters we can use the string by indexed as s[0]
+#  .index("G") gives the index of G
+#  you can also use .replace("","") func for replacing the whole string by any other string
+
+# For numbers we can use many different functions such as
+#  abs() which gives the absolute of a number and pow(1,2) gives 1 raised to 2
+#   max(1,2) an min(1,2) are some basic functions
+# some other functions are round() and floor() and ceil() and sqrt()
+# input("prompt") for input
+
+#Lists
+#friends = [1,2,3] can be any data type of lists you can access the list by starting from [0] or by [-1]
+# use print(:) for range in lists
+
